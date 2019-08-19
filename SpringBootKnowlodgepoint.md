54bb27491d288acc9a6dd2e7335b0a36449dd415
ChengYue <chengyue0313@gmail.com>
2019-08-19 23:39:05 +0800

    mybatis configuration

---
+++ SpringBootKnowlodgepoint.md
@@ -0,0 +1,10 @@
+# SpringBoot
+
+## annotation 
+
+1. __@EnableAutoConfiguration__
+
+   ​	annotation auto-configures the beans that are present in the classpath. This simplifies the developers work by guessing the required beans from the classpath and configure it to run the application. 
+
+2. __@RestController__
+3. @RequestMapping()
--- src/main/java/com/seckillingProject/App.java
+++ src/main/java/com/seckillingProject/App.java
@@ -1,16 +1,40 @@
 package com.seckillingProject;
 
+import com.seckillingProject.dao.UserDOMapper;
+import com.seckillingProject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.seckillingProject"})
+@RestController
+@MapperScan("com.seckillingProject.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/")
+    public String home(){
+        UserDO userDo = userDOMapper.selectByPrimaryKey(1);
+        if(userDo == null){
+            return "用户不存在";
+        }else{
+            return userDo.getName();
+        }
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
